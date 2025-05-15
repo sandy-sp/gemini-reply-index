925dc81cecb0d7686533110888af43540a460873
sandy-sp <sandeep.paidipati@gmail.com>
2025-05-14 23:16:10 -0400

    Add main application component with Supabase session context provider

---
+++ pages/_app.tsx
@@ -0,0 +1,15 @@
+// pages/_app.tsx
+import { SessionContextProvider } from '@supabase/auth-helpers-react'
+import { supabase } from '@/lib/supabaseClient'
+import type { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <SessionContextProvider
+      supabaseClient={supabase}
+      initialSession={pageProps.initialSession}
+    >
+      <Component {...pageProps} />
+    </SessionContextProvider>
+  )
+}
