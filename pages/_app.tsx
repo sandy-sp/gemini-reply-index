d675a01819d7f2b77d1d6c6ef100e8071ffcad8c
sandy-sp <sandeep.paidipati@gmail.com>
2025-05-14 23:19:54 -0400

    Add NavBar component to main application layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 // pages/_app.tsx
 import { SessionContextProvider } from '@supabase/auth-helpers-react'
 import { supabase } from '@/lib/supabaseClient'
+import { NavBar } from '@/components/NavBar'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -9,6 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
       supabaseClient={supabase}
       initialSession={pageProps.initialSession}
     >
+      <NavBar />
       <Component {...pageProps} />
     </SessionContextProvider>
   )
